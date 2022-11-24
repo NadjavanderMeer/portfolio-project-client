6bb8c95e67f47006ffb2b54dc39bb30c3721e794
NadjavanderMeer <nadjavandermeer@gmail.com>
2022-11-24 14:30:05 +0100

    created navBar

--- src/components/NavBar.js
+++ src/components/NavBar.js
@@ -1,7 +1,15 @@
+import { NavLink } from "react-router-dom";
+
 export const NavBar = () => {
   return (
     <div>
-      <p>This is the NavBar!</p>
+      <NavLink to="/">Home</NavLink>
+      {" | "}
+      <NavLink to="/babysitters">Babysitters</NavLink>
+      {" | "}
+      <NavLink to="/families">Families</NavLink>
+      {" | "}
+      <NavLink to="/login">Login</NavLink>
     </div>
   );
 };
